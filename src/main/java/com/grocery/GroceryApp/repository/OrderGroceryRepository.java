28f87410d73a72e3e5a3094c188fd6d797e0d9a7
sanjana1801 <68682363+sanjana1801@users.noreply.github.com>
2023-12-06 11:05:49 +0530

    Update OrderGroceryRepository.java

--- src/main/java/com/grocery/GroceryApp/repository/OrderGroceryRepository.java
+++ src/main/java/com/grocery/GroceryApp/repository/OrderGroceryRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.grocery.GroceryApp.entity.Order;
 
+@Repository
 public interface OrderGroceryRepository extends JpaRepository<Order, Long>{
 
 }
